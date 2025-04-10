277ac3d5aa88310341b80c6b6c6ee9487524bdbc
aravind74 <aravindsajeev4@gmail.com>
2025-04-10 15:51:11 -0500

    Initial commit

---
+++ UniSchedAPI/src/main/java/com/project/unischedapi/UniSchedApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.unischedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniSchedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniSchedApiApplication.class, args);
+	}
+
+}
---
+++ UniSchedAPI/src/test/java/com/project/unischedapi/UniSchedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.unischedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniSchedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
